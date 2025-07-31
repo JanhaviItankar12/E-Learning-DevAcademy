b279ceab18367f802228d2f2577741fa8cad520b
JanhaviItankar12 <itankarjanvi@gmail.com>
2025-07-31 11:49:32 +0530

    done upto courseProgress page but facing issue with rendering

--- Client/Client/src/Pages/student/CourseProgress.jsx
+++ Client/Client/src/Pages/student/CourseProgress.jsx
@@ -1,35 +1,94 @@
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardTitle } from '@/components/ui/card'
-import { useGetCourseProgressQuery } from '@/features/api/courseProgressApi';
-import { CheckCircle2, CirclePlay } from 'lucide-react';
-import React from 'react'
+import { useCompleteCourseMutation, useGetCourseProgressQuery, useIncompleteCourseMutation, useUpdateLectureProgressMutation } from '@/features/api/courseProgressApi';
+import { CheckCircle, CheckCircle2, CirclePlay } from 'lucide-react';
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom';
 import { useState } from 'react';
+import { toast } from 'sonner';
 
 
 const CourseProgress = () => {
-    const isCompleted = true;
+   
     const params = useParams();
     const courseId = params.courseId;
     const { data, isLoading, isError, refetch } = useGetCourseProgressQuery(courseId);
-
+    
+    const [updateLectureProgress]=useUpdateLectureProgressMutation();
+    const [completeCourse,{data:markAsCompletedData,isSuccess:completedSuccess}]=useCompleteCourseMutation();
+    const [incompleteCourse,{data:markAsInCompletedData,isSuccess:inCompletedSuccess}]=useIncompleteCourseMutation();
+    
+   
+    
     const [currentLecture, setCurrentLecture] = useState(null);
 
+    useEffect(() => {
+        if (data?.data?.courseDetails?.lectures?.length && !currentLecture) {
+            setCurrentLecture(data.data.courseDetails.lectures[0]);
+        }
+    }, [data, currentLecture]);
+    
     if (isLoading) return <p>Loading...</p>
     if (isError) return <p>Failed to fetch course details</p>
-    console.log(data);
+    if(!data?.data?.courseDetails?.lectures?.length) return <p>No lectures found for this course.</p>
+
+    
+    
     const { courseDetails, progress, completed } = data.data;
     const { courseTitle } = courseDetails;
 
     // inintialize the first lecture if not exist
     const initialLecture = currentLecture || courseDetails.lectures && courseDetails.lectures[0]
+    
+    const isLectureCompleted=(lectureId)=>{
+        return progress.some((prog)=>prog.lectureId===lectureId && prog.viewed) 
+    }
+
+    // handle select  a specific lecture to watch
+    const handleSelectLecture=(lecture)=>{
+        setCurrentLecture(lecture)
+    }
+
+    const handleLectureProgress=async(lectureId)=>{
+         await updateLectureProgress({courseId,lectureId});
+         refetch();
+    }
+
+    const handleCompleteCourse=async()=>{
+       await completeCourse(courseId);
+    }
+
+    const handleInCompleteCourse=async()=>{
+       await incompleteCourse(courseId);
+    }
+
+    useEffect(() => {
+        if (completedSuccess) {
+            toast.success(markAsCompletedData?.message || "Course marked as completed");
+            refetch();
+        }
+        if (inCompletedSuccess) {
+            toast.success(markAsInCompletedData?.message || "Course marked as incomplete");
+            refetch();
+        }
+    },[completedSuccess, inCompletedSuccess, markAsCompletedData, markAsInCompletedData, refetch]);
+
+
+
+
     return (
         <div className='max-w-7xl mx-auto p-4 mt-20'>
             {/* display course name */}
             <div className='flex justify-between mb-4'>
                 <h1 className='text-2xl font-bold'>{courseTitle}</h1>
-                <Button>Completed</Button>
+                <Button variant={completed? "outline" :"default"} onClick={completed ? handleInCompleteCourse : handleCompleteCourse}>
+                    {
+                        completed? <div className='flex items-center'><CheckCircle className='h-4 w-4 mr-2'/><span>Completed</span> </div>:
+                     "Mark as Completed"
+            }
+
+            </Button>
             </div>
 
             {/* video section */}
@@ -40,12 +99,13 @@ const CourseProgress = () => {
                             src={currentLecture?.videoUrl || initialLecture.videoUrl}
                             controls
                             className="w-full h-full object-contain rounded-lg"
+                            onPlay={()=>handleLectureProgress(currentLecture?._id)}
                         />
                     </div>
 
                     {/* Display current watching title */}
                     <div className='mt-2'>
-                        <h3 className='font-medium text-lg'>{`Lecture ${courseDetails.lectures.findIndex((lecture)=>lecture._id===(currentLecture?._id || initialLecture._id))+1} : ${currentLecture?.lectureTitle || initialLecture?.lectureTitle}`}</h3>
+                        <h3 className='font-medium text-lg'>{`Lecture ${courseDetails.lectures.findIndex((lecture)=>lecture._id===currentLecture?._id )+1} : ${currentLecture?.lectureTitle}`}</h3>
                     </div>
                 </div>
                 {/* lecture sideBar */}
@@ -54,11 +114,11 @@ const CourseProgress = () => {
                     <div className='flex-1 overflow-y-auto'>
                         {
                             courseDetails?.lectures.map((lecture) => (
-                                <Card key={lecture._id} className={"mb-3 hover:cursor-pointer transition transform "}>
+                                <Card key={lecture._id} onClick={()=>handleSelectLecture(lecture)} className={`mb-3 hover:cursor-pointer transition transform ${lecture._id===currentLecture?._id  ? 'bg-gray-200' :'dark:bg-gray-800'}`}>
                                     <CardContent className={'flex items-center justify-between p-4'}>
                                         <div className='flex items-center'>
                                             {
-                                                isCompleted ? (<CheckCircle2 size={24} className='text-green-500 mr-2' />) :
+                                               isLectureCompleted(lecture._id) ? (<CheckCircle2 size={24} className='text-green-500 mr-2' />) :
                                                     (<CirclePlay size={24} className='text-gray-500 mr-2' />)
                                             }
 
@@ -66,7 +126,12 @@ const CourseProgress = () => {
                                                 <CardTitle className={'text-lg font-medium'}>{lecture.lectureTitle}</CardTitle>
                                             </div>
                                         </div>
-                                        <Badge variant={'outline'} className={'bg-green-200 text-green-600'}>Completed</Badge>
+                                        {
+                                            isLectureCompleted(lecture._id) && (
+                                             <Badge variant={'outline'} className={'bg-green-200 text-green-600'}>Completed</Badge>
+                                            )
+                                        }
+                                        
                                     </CardContent>
                                 </Card>
                             ))
